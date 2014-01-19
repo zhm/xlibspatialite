6cacaf9a1c9c0a7ff98a03ee1185913503f11346
Zac McCormick <zac.mccormick@gmail.com>
2014-01-18 21:45:11 -0500

    Generate a public directory for use in a project that includes headers and libs.

--- build.sh
+++ build.sh
@@ -15,8 +15,8 @@ SIMULATOR_BUILD_DIR_DEBUG='./build/Debug-simulator'
 UNIVERSAL_BUILD_DIR_RELEASE='./build/Release-universal'
 UNIVERSAL_BUILD_DIR_DEBUG='./build/Debug-universal'
 
-rm -rf $UNIVERSAL_BUILD_DIR_RELEASE
-rm -rf $UNIVERSAL_BUILD_DIR_DEBUG
+# rm -rf $UNIVERSAL_BUILD_DIR_RELEASE
+# rm -rf $UNIVERSAL_BUILD_DIR_DEBUG
 
 mkdir -p $DEVICE_BUILD_DIR_RELEASE
 mkdir -p $DEVICE_BUILD_DIR_DEBUG
@@ -25,11 +25,11 @@ mkdir -p $SIMULATOR_BUILD_DIR_DEBUG
 mkdir -p $UNIVERSAL_BUILD_DIR_RELEASE
 mkdir -p $UNIVERSAL_BUILD_DIR_DEBUG
 
-xcodebuild -configuration Release -scheme libspatialite -sdk $DEVICE CONFIGURATION_BUILD_DIR=$DEVICE_BUILD_DIR_RELEASE
-xcodebuild -configuration Debug -scheme libspatialite -sdk $DEVICE CONFIGURATION_BUILD_DIR=$DEVICE_BUILD_DIR_DEBUG
+# xcodebuild -configuration Release -scheme libspatialite -sdk $DEVICE CONFIGURATION_BUILD_DIR=$DEVICE_BUILD_DIR_RELEASE
+# xcodebuild -configuration Debug -scheme libspatialite -sdk $DEVICE CONFIGURATION_BUILD_DIR=$DEVICE_BUILD_DIR_DEBUG
 
-xcodebuild -configuration Release -scheme libspatialite -sdk $SIMULATOR CONFIGURATION_BUILD_DIR=$SIMULATOR_BUILD_DIR_RELEASE
-xcodebuild -configuration Debug -scheme libspatialite -sdk $SIMULATOR CONFIGURATION_BUILD_DIR=$SIMULATOR_BUILD_DIR_DEBUG
+# xcodebuild -configuration Release -scheme libspatialite -sdk $SIMULATOR CONFIGURATION_BUILD_DIR=$SIMULATOR_BUILD_DIR_RELEASE
+# xcodebuild -configuration Debug -scheme libspatialite -sdk $SIMULATOR CONFIGURATION_BUILD_DIR=$SIMULATOR_BUILD_DIR_DEBUG
 
 # xcodebuild -configuration Debug -target ${TARGET} -sdk ${DEVICE} ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO
 # xcodebuild -configuration Debug -target ${TARGET} -sdk ${SIMULATOR} ${ACTION} RUN_CLANG_STATIC_ANALYZER=NO
@@ -54,3 +54,22 @@ lipo -create -output "${UNIVERSAL_BUILD_DIR_RELEASE}/libgeos.a" "${DEVICE_BUILD_
 lipo -create -output "${UNIVERSAL_BUILD_DIR_RELEASE}/liblwgeom.a" "${DEVICE_BUILD_DIR_RELEASE}/liblwgeom.a" "${SIMULATOR_BUILD_DIR_RELEASE}/liblwgeom.a"
 lipo -create -output "${UNIVERSAL_BUILD_DIR_RELEASE}/libproj.a" "${DEVICE_BUILD_DIR_RELEASE}/libproj.a" "${SIMULATOR_BUILD_DIR_RELEASE}/libproj.a"
 lipo -create -output "${UNIVERSAL_BUILD_DIR_RELEASE}/libspatialite.a" "${DEVICE_BUILD_DIR_RELEASE}/libspatialite.a" "${SIMULATOR_BUILD_DIR_RELEASE}/libspatialite.a"
+
+rm -rf ./public
+
+mkdir -p ./public
+
+cp ./sqlite3/redefines.h ./public/redefines.h
+cp ./sqlite3/sqlite3.h ./public/spatialite_sqlite3.h
+cp ./sqlite3/sqlite3ext.h ./public/spatialite_sqlite3ext.h
+cp ./spatialite/headers/spatialite.h ./public/spatialite.h
+
+mkdir -p ./public/spatialite
+cp -R ./spatialite/headers/spatialite/* ./public/spatialite
+
+mkdir ./public/lib
+
+cp "${UNIVERSAL_BUILD_DIR_RELEASE}/libgeos.a" ./public/lib
+cp "${UNIVERSAL_BUILD_DIR_RELEASE}/liblwgeom.a" ./public/lib
+cp "${UNIVERSAL_BUILD_DIR_RELEASE}/libproj.a" ./public/lib
+cp "${UNIVERSAL_BUILD_DIR_RELEASE}/libspatialite.a" ./public/lib
